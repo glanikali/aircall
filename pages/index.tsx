ba46cd955df29dcfe46518750b48bd69149dd97a
Glani K <glani.kali@gmail.com>
2022-07-15 16:30:22 -0400

    changed view to full height

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,9 +25,9 @@ const Home = ({ fallback }: Props) => {
     ? fallback['/activities']
     : data?.filter((el: CallObject) => el.is_archived === false);
   return (
-    <div className="bg-zinc-50 px-2 sm:py-4 sm:px-0">
-      <ContentWrapper>
-        <div className="bg-white p-4 shadow-sm rounded-lg">
+    <div className="bg-zinc-50 px-1 sm:py-0 sm:px-0 ">
+      <ContentWrapper className="min-h-[calc(100vh_-_74px)]">
+        <div className="bg-white p-4 shadow-sm rounded-lg  sm:mt-2">
           <div className="grid gap-4 grid-cols-1">
             {callList?.map((el, i) => (
               <Listable key={i} callObject={el} />
